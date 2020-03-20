cb3aa1d85d430d066b3b93ecbdd980548d41a6f2
cristhian2019 <cristhian@idp.com>
2020-03-19 21:48:48 -0600

    Método para arreglar array

--- src/arreglos/Arreglos.java
+++ src/arreglos/Arreglos.java
@@ -19,6 +19,8 @@ public class Arreglos {
             MiArray.Agregar(i, lee.nextInt());
         }
         MiArray.mostrarArray();
+        MiArray.OrdenarAscendente();
+        MiArray.mostrarArray();
     }
     
 }
--- src/arreglos/ClaseArreglos.java
+++ src/arreglos/ClaseArreglos.java
@@ -15,7 +15,7 @@ public class ClaseArreglos {
         {
             this.array = new int[tamanio];
         }
-        
+        //Método para mostrar el array
         public void mostrarArray(){
             for(int i=0; i<=this.array.length-1; i++){
                 System.out.print((i<this.array.length-1) ? this.array[i] + "," : this.array[i]);
@@ -26,7 +26,22 @@ public class ClaseArreglos {
         public int getTamanio(){
             return this.array.length;
         }
+        //Método para agregar elementos al array
         public void Agregar(int posicion,int elemento){
              this.array[posicion] = elemento;
+        }
+        
+        //Métodos para ordenar vector ascendentemente
+        public void OrdenarAscendente(){
+            int aux;
+            for(int i=0; i<this.array.length-1; i++){
+                for(int j=0; j<this.array.length-1; j++){
+                    if(this.array[j] > this.array[j+1]){
+                        aux = this.array[j];
+                        this.array[j] = this.array[j+1];
+                        this.array[j+1] = aux;
+                    }
+                }
+            }
         }
 }
